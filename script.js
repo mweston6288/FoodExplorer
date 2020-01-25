699a02db4325ca989aac0395c04cdebcca598adc
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-01-25 11:46:46 -0500

    added some comments

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// API keys that may be useful
 var APIKeyNutritionix = "543062e2f89a916d71461f4c8c3befda"
 var APIKeyUSDA = "L3tHPcFfPTSKSiXbCfKg2KltRijm4Dlj6PL2hK7I"
 
 
-
+// backend test code
 var input = "butter"
+
 $.ajax({
     url: "https://api.nal.usda.gov/fdc/v1/search?api_key="+APIKeyUSDA+"\&generalSearchInput="+input,
 
