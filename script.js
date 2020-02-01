0839da658a5eacb8ea3f1c88fa7196ddde345049
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-01-31 21:14:41 -0500

    Saving

--- script.js
+++ script.js
@@ -25,12 +25,12 @@ function makeCalls(){
         console.log(result)
         
         $(result).each(function(index, element){
-        // Spoonacular recipe call by ID. Currently hard-coded to only get item 0
-        $.ajax({
-            url: "https://api.spoonacular.com/recipes/"+result[0].id+"/information?includeNutrition=true&apiKey="+APIKeySpoonacular,
-            method: "GET"
-        }).then(function(result){
-            console.log(result)
+            // Spoonacular recipe call by ID. Currently hard-coded to only get item 0
+            $.ajax({
+                url: "https://api.spoonacular.com/recipes/"+result[0].id+"/information?includeNutrition=true&apiKey="+APIKeySpoonacular,
+                method: "GET"
+            }).then(function(result){
+                console.log(result)
     })})})
 }
 $("#searchBar").on("click", function(){
